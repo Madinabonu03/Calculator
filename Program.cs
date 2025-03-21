da345e2846d88acad67f7c75a125e281872eb30d
Madinabonu03 <odiljonovamadina11@gmail.com>
2025-03-21 07:43:38 +0300

    First upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+       
+
+ class Program{
+    static void Main() 
+    {
+        string firstInput =Console.ReadLine();
+        string secondInput = Console.ReadLine();
+int firstNumber=Convert.ToInt32(firstInput);
+  int   secondNumber=Convert.ToInt32(secondInput);
+        System.Console.WriteLine($"Natija:{firstNumber+secondNumber}");
+
+
+   
+}
+    }
+        
